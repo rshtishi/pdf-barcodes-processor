8c8d08bc0f40ae03a234731d5187f9b6f3f3cf00
rshtishi <rshtishi@3dial.eu>
2019-12-14 12:25:16 +0100

    Enhacing Zxing decoding barcodes

--- src/main/java/al/tirana/pdfBarcodesProcessor/barcodeDecoder/ZxingDecoder.java
+++ src/main/java/al/tirana/pdfBarcodesProcessor/barcodeDecoder/ZxingDecoder.java
@@ -1,29 +1,28 @@
 package al.tirana.pdfBarcodesProcessor.barcodeDecoder;
 
 import java.awt.image.BufferedImage;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
 import com.google.zxing.Reader;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.oned.Code39Reader;
 
 public class ZxingDecoder implements BarcodeDecoder {
 
 	@Override
 	public String decode(BufferedImage image,ZxingDecoderType type){
 		Reader reader =  ZxingFactory.getInstance().getBarcodeReader(type);
-		Hashtable<DecodeHintType, Object> decodeHintTypes = new Hashtable<DecodeHintType, Object>();
+		Map<DecodeHintType, Object> decodeHintTypes = new HashMap<DecodeHintType, Object>();
 		decodeHintTypes.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
 		LuminanceSource source = new BufferedImageLuminanceSource(image);
 		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
 		try {
-			Result result = reader.decode(bitmap);
+			Result result = reader.decode(bitmap,decodeHintTypes);
 			return result.getText();
 		} catch (Exception e) {
 		}
--- src/main/java/al/tirana/pdfBarcodesProcessor/barcodeDecoder/ZxingDecoderType.java
+++ src/main/java/al/tirana/pdfBarcodesProcessor/barcodeDecoder/ZxingDecoderType.java
@@ -1,18 +1,30 @@
 package al.tirana.pdfBarcodesProcessor.barcodeDecoder;
 
+import com.google.zxing.BarcodeFormat;
+
 public enum ZxingDecoderType {
 	
-	CODE_39,
-	CODE_93,
-	CODE_128,
-	CODABAR,
-	RSS_14,
-	RSS_EXPANDED,
-	ITF,
-	UPCA,
-	UPCE,
-	EAN_13,
-	EAN_8,
-	DEFAULT;
+	CODE_39(BarcodeFormat.CODE_39),
+	CODE_93(BarcodeFormat.CODE_93),
+	CODE_128(BarcodeFormat.CODE_128),
+	CODABAR(BarcodeFormat.CODABAR),
+	RSS_14(BarcodeFormat.RSS_14),
+	RSS_EXPANDED(BarcodeFormat.RSS_EXPANDED),
+	ITF(BarcodeFormat.ITF),
+	UPCA(BarcodeFormat.UPC_A),
+	UPCE(BarcodeFormat.UPC_E),
+	EAN_13(BarcodeFormat.EAN_13),
+	EAN_8(BarcodeFormat.EAN_8),
+	DEFAULT(null);
+	
+	private BarcodeFormat barcodeFormat;
+	
+	ZxingDecoderType(BarcodeFormat barcodeFormat) {
+		this.barcodeFormat = barcodeFormat;
+	}
+	
+	public BarcodeFormat getBarcodeFormat() {
+		return barcodeFormat;
+	}
 	
 }
