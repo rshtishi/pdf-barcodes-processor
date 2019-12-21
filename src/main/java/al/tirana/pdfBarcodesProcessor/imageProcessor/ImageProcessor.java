cd923487383381c72d4322d56acbe94f3227c630
rshtishi <rshtishi@3dial.eu>
2019-12-21 12:11:41 +0100

    Implementing the module for processing the images

--- src/main/java/al/tirana/pdfBarcodesProcessor/imageProcessor/ImageProcessor.java
+++ src/main/java/al/tirana/pdfBarcodesProcessor/imageProcessor/ImageProcessor.java
@@ -1,13 +1,40 @@
 package al.tirana.pdfBarcodesProcessor.imageProcessor;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
+/**
+ * 
+ * @author Rando Shtishi
+ *
+ */
 public interface ImageProcessor {
 
-	BufferedImage extractBarcodeImage(BufferedImage image);
-	
-	
-	
-	BufferedImage rotateImage(BufferedImage image,double angle);
+	/**
+	 * @param image
+	 * @return It crop all area where bar code images are located and returns the
+	 *         list of bar code images. To be used when image might have more that
+	 *         one image.
+	 */
+	List<BufferedImage> extractBarcodeImages(BufferedImage image);
+
+	/**
+	 * 
+	 * @param image
+	 * @return It crop are where the bar code image is located. It return the bar
+	 *         code image along with information if the bar code image is skewed. If
+	 *         it is not skewed no rotation it is need or 180 degree rotation it is
+	 *         needed. If it is skewed , it is need to try rotating with different
+	 *         angles.
+	 */
+	BarcodeImage extractBarcodeImage(BufferedImage image);
+
+	/**
+	 * 
+	 * @param image
+	 * @param angle
+	 * @return Rotates the image to a specific angle.
+	 */
+	BufferedImage rotateImage(BufferedImage image, double angle);
 
 }
--- src/main/java/al/tirana/pdfBarcodesProcessor/imageProcessor/OpenCVHelper.java
+++ src/main/java/al/tirana/pdfBarcodesProcessor/imageProcessor/OpenCVHelper.java
@@ -3,12 +3,28 @@ package al.tirana.pdfBarcodesProcessor.imageProcessor;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.util.Iterator;
+import java.util.List;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
+import org.opencv.imgproc.Imgproc;
 
+/**
+ * 
+ * @author Rando Shtishi
+ *
+ */
 public class OpenCVHelper {
 
+	/**
+	 * @param mat
+	 * @return The Mat type image is converted to BufferedImage type.
+	 */
 	public static BufferedImage mat2Img(Mat mat) {
 		BufferedImage image = new BufferedImage(mat.width(), mat.height(), BufferedImage.TYPE_3BYTE_BGR);
 		WritableRaster raster = image.getRaster();
@@ -18,11 +34,77 @@ public class OpenCVHelper {
 		return image;
 	}
 
+	/**
+	 * 
+	 * @param image
+	 * @return The BufferedImage type image is converted to Mat type.
+	 */
 	public static Mat img2Mat(BufferedImage image) {
 		byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 		Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
 		mat.put(0, 0, data);
 		return mat;
+	}
+
+	/**
+	 * 
+	 * @param contours
+	 * @param imageMat
+	 * @return Estimates the crop area and return a rectangle area. This rectangle
+	 *         area is area that will be cropped from the image.
+	 */
+	public static Rect estimateCropRect(List<MatOfPoint> contours, Mat imageMat) {
+		double maxArea = 0;
+		MatOfPoint max_contour = new MatOfPoint();
+		Iterator<MatOfPoint> iterator = contours.iterator();
+		while (iterator.hasNext()) {
+			MatOfPoint contour = iterator.next();
+			double area = Imgproc.contourArea(contour);
+			if (area > maxArea) {
+				maxArea = area;
+				max_contour = contour;
+			}
+		}
+		RotatedRect rect = Imgproc.minAreaRect(new MatOfPoint2f(max_contour.toArray()));
+		Rect cropRect = rect.boundingRect();
+		return cropRect;
+	}
+
+	/**
+	 * 
+	 * @param cropRect
+	 * @param imageMat
+	 * @return Validates the crop area goes outside of boundary of image.
+	 */
+	public static boolean validateCropRect(Rect cropRect, Mat imageMat) {
+		boolean validation = true;
+		if (cropRect.x < 0)
+			validation = false;
+		if (cropRect.y < 0)
+			validation = false;
+		if (cropRect.width > imageMat.cols() - cropRect.x)
+			validation = false;
+		if (cropRect.height > imageMat.rows() - cropRect.y)
+			validation = false;
+		return validation;
+	}
+
+	/**
+	 * 
+	 * @param cropRect
+	 * @param imageMat
+	 * @return Fixes the crop area if it goes outside the boundary of image.
+	 */
+	public static Rect fixCropRect(Rect cropRect, Mat imageMat) {
+		if (cropRect.x < 0)
+			cropRect.x = 0;
+		if (cropRect.y < 0)
+			cropRect.y = 0;
+		if (cropRect.width > imageMat.cols() - cropRect.x)
+			cropRect.width = imageMat.width() - cropRect.x;
+		if (cropRect.height > imageMat.rows() - cropRect.y)
+			cropRect.height = imageMat.height() - cropRect.y;
+		return cropRect;
 	}
 
 }
