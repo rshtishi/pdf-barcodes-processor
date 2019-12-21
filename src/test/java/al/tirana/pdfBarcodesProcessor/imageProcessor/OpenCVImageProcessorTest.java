57c8f1d8f64b395c9edb327321003e298348112c
rshtishi <rshtishi@3dial.eu>
2019-12-21 12:12:25 +0100

    Test class for image processing module

--- src/test/java/al/tirana/pdfBarcodesProcessor/imageProcessor/OpenCVImageProcessorTest.java
+++ src/test/java/al/tirana/pdfBarcodesProcessor/imageProcessor/OpenCVImageProcessorTest.java
@@ -1,17 +1,23 @@
 package al.tirana.pdfBarcodesProcessor.imageProcessor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * 
+ * @author Rando Shtishi
+ *
+ */
 public class OpenCVImageProcessorTest {
 
 	private OpenCVImageProcessor imageProcessor;
@@ -21,6 +27,25 @@ public class OpenCVImageProcessorTest {
 		imageProcessor = new OpenCVImageProcessor();
 	}
 
+	/**
+	 * @throws Exception Testing extraction bar code images from image containing
+	 *                   multiple bar codes. Not Finished.
+	 */
+	@Test
+	public void testExtractBarcodeImages() throws Exception {
+		// setup
+		String imagePath = "src/test/resources/img-test5.jpg";
+		BufferedImage image = ImageIO.read(new File(imagePath));
+		// execute
+		List<BufferedImage> imageListResult = imageProcessor.extractBarcodeImages(image);
+		// verify
+		ImageIO.write(imageListResult.get(0), "jpg", new File("src/test/resources/img-barcode-detected.jpg"));
+	}
+
+	/**
+	 * @throws Exception Testing the extraction a bar code image from image
+	 *                   containing a single bar code.
+	 */
 	@Test
 	public void testExtractBarcodeImage() throws Exception {
 		// setup
@@ -32,40 +57,38 @@ public class OpenCVImageProcessorTest {
 		BufferedImage image3 = ImageIO.read(new File(imagePath3));
 		String imagePath4 = "src/test/resources/img-test4.png";
 		BufferedImage image4 = ImageIO.read(new File(imagePath4));
+		String imagePath5 = "src/test/resources/img-test5.jpg";
+		BufferedImage image5 = ImageIO.read(new File(imagePath5));
 		// execute
-		BufferedImage imageResult1 = imageProcessor.extractBarcodeImage(image1);
-		BufferedImage imageResult2 = imageProcessor.extractBarcodeImage(image2);
-		BufferedImage imageResult3 = imageProcessor.extractBarcodeImage(image3);
-		BufferedImage imageResult4 = imageProcessor.extractBarcodeImage(image4);
+		BarcodeImage imageResult1 = imageProcessor.extractBarcodeImage(image1);
+		BarcodeImage imageResult2 = imageProcessor.extractBarcodeImage(image2);
+		BarcodeImage imageResult3 = imageProcessor.extractBarcodeImage(image3);
+		BarcodeImage imageResult4 = imageProcessor.extractBarcodeImage(image4);
+		BarcodeImage imageResult5 = imageProcessor.extractBarcodeImage(image5);
 		// verify
-		ImageIO.write(imageResult1, "jpg", new File("src/test/resources/img-barcode-extracted1.jpg"));
-		assertEquals(imageResult1.getWidth(), 411);
-		assertEquals(imageResult1.getHeight(), 328);
-		ImageIO.write(imageResult2, "jpg", new File("src/test/resources/img-barcode-extracted2.jpg"));
-		assertEquals(imageResult2.getWidth(), 628);
-		assertEquals(imageResult2.getHeight(), 139);
-		ImageIO.write(imageResult3, "jpg", new File("src/test/resources/img-barcode-extracted3.jpg"));
-		assertEquals(imageResult3.getWidth(), 258);
-		assertEquals(imageResult3.getHeight(), 84);
-		ImageIO.write(imageResult4, "jpg", new File("src/test/resources/img-barcode-extracted4.jpg"));
-		assertEquals(imageResult4.getWidth(), 973);
-		assertEquals(imageResult4.getHeight(), 243);
+		ImageIO.write(imageResult1.getImage(), "jpg", new File("src/test/resources/img-barcode-extracted1.jpg"));
+		assertEquals(imageResult1.getImage().getWidth(), 411);
+		assertEquals(imageResult1.getImage().getHeight(), 328);
+		assertTrue(imageResult1.isSkewed());
+		ImageIO.write(imageResult2.getImage(), "jpg", new File("src/test/resources/img-barcode-extracted2.jpg"));
+		assertEquals(imageResult2.getImage().getWidth(), 628);
+		assertEquals(imageResult2.getImage().getHeight(), 139);
+		assertFalse(imageResult2.isSkewed());
+		ImageIO.write(imageResult3.getImage(), "jpg", new File("src/test/resources/img-barcode-extracted3.jpg"));
+		assertEquals(imageResult3.getImage().getWidth(), 258);
+		assertEquals(imageResult3.getImage().getHeight(), 84);
+		assertFalse(imageResult3.isSkewed());
+		ImageIO.write(imageResult4.getImage(), "jpg", new File("src/test/resources/img-barcode-extracted4.jpg"));
+		assertEquals(imageResult4.getImage().getWidth(), 973);
+		assertEquals(imageResult4.getImage().getHeight(), 243);
+		assertTrue(imageResult4.isSkewed());
+		ImageIO.write(imageResult5.getImage(), "jpg", new File("src/test/resources/img-barcode-extracted5.jpg"));
+		assertFalse(imageResult5.isSkewed());
 	}
 
-	@Test
-	public void testDetectBarcodeImageUsingClassifier() throws Exception {
-		// setup
-		String imagePath = "src/test/resources/img-test5.jpg";
-		BufferedImage image = ImageIO.read(new File(imagePath));
-		// execute
-		Method method = imageProcessor.getClass().getDeclaredMethod("detectBarcodeImageUsingClassifier",
-				java.awt.image.BufferedImage.class);
-		method.setAccessible(true);
-		BufferedImage imageResult = (BufferedImage) method.invoke(imageProcessor, image);
-		// verify
-		ImageIO.write(imageResult, "jpg", new File("src/test/resources/img-barcode-detected.jpg"));
-	}
-
+	/**
+	 * @throws Exception Testing the rotating of image.
+	 */
 	@Test
 	public void testRotateImage() throws Exception {
 		// setup
