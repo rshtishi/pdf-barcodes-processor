bfbe30451b041d62e064920680f2ee847109955f
rshtishi <rshtishi@3dial.eu>
2020-01-24 15:12:38 +0100

    modifying the pdf page

--- src/main/java/al/tirana/pdfBarcodesProcessor/pdfprocessor/PdfPage.java
+++ src/main/java/al/tirana/pdfBarcodesProcessor/pdfprocessor/PdfPage.java
@@ -1,9 +1,14 @@
 package al.tirana.pdfBarcodesProcessor.pdfprocessor;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 /**
  * 
@@ -20,6 +25,9 @@ public class PdfPage {
 	public Map<String, BufferedImage> getDecodedBarcodeImageMap() {
 		return decodedBarcodeImageMap;
 	}
+	
+	private PdfPage() {
+	}
 
 	public int getPageNumber() {
 		return pageNumber;
@@ -31,6 +39,40 @@ public class PdfPage {
 
 	public String getPageName() {
 		return pageName;
+	}
+	
+	/**
+	 * Returns the list with all images that are decoded.
+	 * @return
+	 */
+	public List<BufferedImage> getAllDecodedBarcodeImages(){
+		return new ArrayList<>(decodedBarcodeImageMap.values());
+	}
+	
+	/**
+	 * Returns the list with all text from decoded bar codes
+	 * @return
+	 */
+	public List<String> getAllDecodedBarcodesList(){
+		return new ArrayList<>(decodedBarcodeImageMap.keySet());
+	}
+	
+	/**
+	 * Saves all decoded bar codes images inside a directory specified in parameter,
+	 * where image name is bar code decoded text.
+	 * @param dirPath
+	 */
+	public void saveDecodedBarcodeImages(String dirPath) {
+		decodedBarcodeImageMap.keySet().forEach(name -> {
+			String filePath = dirPath+"/"+name+".jpg";
+			BufferedImage image = decodedBarcodeImageMap.get(name);
+			try {
+				ImageIO.write(image, "png", new File(filePath));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		});
 	}
 
 	public static class Builder {
---
+++ src/test/java/al/tirana/pdfBarcodesProcessor/pdfprocessor/PdfPageTest.java
@@ -0,0 +1,54 @@
+package al.tirana.pdfBarcodesProcessor.pdfprocessor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PdfPageTest {
+	
+	private PdfPage pdfPage;
+	
+	@Before()
+	public void setup() throws IOException {
+		pdfPage =new PdfPage.Builder().build();
+		BufferedImage image = ImageIO.read(new File("src/test/resources/img-test1.jpg"));
+		pdfPage.getDecodedBarcodeImageMap().put("test", image);
+	}
+
+	@Test
+	public void testSaveDecodedBarcodeImages() {
+		//execute
+		pdfPage.saveDecodedBarcodeImages("src/test/resources");
+		//verify
+		File file = new File("src/test/resources/test.jpg");
+		assertTrue(file.exists());
+	}
+	
+	@Test
+	public void testGetAllDecodedBarcodeImages() {
+		//execute
+		List<BufferedImage> images = pdfPage.getAllDecodedBarcodeImages();
+		//verify
+		int expected = 1;
+		assertEquals(expected, images.size());
+	}
+	
+	@Test
+	public void testGetAllDecodedBarcodesList() {
+		//execute
+		List<BufferedImage> decodedBarcodes = pdfPage.getAllDecodedBarcodeImages();
+		//verify
+		int expected = 1;
+		assertEquals(expected, decodedBarcodes.size());
+	}
+
+}
